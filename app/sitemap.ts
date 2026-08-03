1b80d7d1daaec1f3784e5c9ac82fddce0a7afcc1
hadukpara-design <hadukpara@gmail.com>
2026-08-04 01:28:54 +0530

    Hardcode propity.in in sitemap to fix localhost URL issue

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const base = process.env.NEXT_PUBLIC_SITE_URL || 'https://propity.in'
+  const base = 'https://propity.in'
   const now = new Date()
 
   return [
